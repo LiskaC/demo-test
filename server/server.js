d6101f0dcac9a33d268f42c20b36b85175f109fb
Liska Crofts <38409100+LiskaC@users.noreply.github.com>
2024-03-24 19:23:53 +0100

    DT-4: Create a live server to send requests to
    The frontend ought to be able to fetch the user, add a server for
    this.

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import http from 'http'
+
+const server = http.createServer((req, res) => {
+  let requestBody = '';
+
+  req.on('data', (chunk) => {
+    requestBody += chunk;
+  });
+
+  req.on('end', () => {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+
+    const response = {
+      headers: req.headers,
+      body: {
+        username: 'LISKA',
+        password: 'Not secure'
+      },
+    }
+
+    res.end(JSON.stringify(response))
+  })
+})
+
+const PORT = 3000;
+const HOST = 'localhost';
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+})
